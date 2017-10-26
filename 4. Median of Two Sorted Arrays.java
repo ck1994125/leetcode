f01e2165f4b3da80d81166ba1c9412c8c2e3f270
ck1994125 <ck3621863@163.com>
2017-10-26 22:41:10 +0800

    modify

--- 4. Median of Two Sorted Arrays.java
+++ 4. Median of Two Sorted Arrays.java
@@ -7,7 +7,9 @@ There are two sorted arrays nums1 and nums2 of size m and n respectively.
 Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 
 分析：
-
+    我们比较A[k/2-1]和B[k/2-1]两个元素，这两个元素分别表示A的第k/2小的元素和B的第k/2小的元素。
+    这两个元素比较共有三种情况：>、<和=:
+    如果A[k/2-1]<B[k/2-1]，这表示A[0]到A[k/2-1]的元素都在A和B合并之后的前k小的元素中。换句话说，A[k/2-1]不可能大于两数组合并之后的第k小值，所以我们可以将其抛弃。
 */
 class Solution {
    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
