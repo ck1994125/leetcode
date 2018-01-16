063c59657a8d1274953dd4f8800a48ad11ffda35
ck1994125 <ck3621863@163.com>
2018-01-16 12:21:05 +0800

    stack

--- 20. Valid Parentheses.java
+++ 20. Valid Parentheses.java
@@ -12,49 +12,17 @@ The brackets must close in the correct order, "()" and "()[]{}" are all valid bu
 */
 class Solution {
     public boolean isValid(String s) {
-        if (s == null || s.length() % 2 == 1) {
-            return false;
-        }
-
-        Stack<Character> stack = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                stack.push(s.charAt(i));
-            }
-            else{
-                if (stack.empty()) {
-                    return false;
-                }
-                if (s.charAt(i) == ')') {
-                    char c = stack.pop();
-                    if (c != '(') {
-                        return false;
-                    }
-                }
-                else if (s.charAt(i) == ']') {
-                    char c = stack.pop();
-                    if (c != '[') {
-                        return false;
-                    }
-                }
-                else if (s.charAt(i) == '}') {
-                    char c = stack.pop();
-                    if (c != '{') {
-                        return false;
-                    }
-                }
-                else {
-                    return false;
-                }
-            }
-
-        }
-        if (stack.empty()) {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+}
 }
